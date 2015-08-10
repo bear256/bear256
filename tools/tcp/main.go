fb92c91daaeea54d0e38f7a6f43c379261502d7d
Panda <bear256@qq.com>
2015-08-11 01:57:29 +0800

    Create tools for Autogen Site

---
+++ tools/tcp/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/miguel-branco/goconfig"
+	"log"
+)
+
+func readCfg(cfg string) {
+	c, err := goconfig.ReadConfigFile(cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Site
+	charset, _ := c.GetString("site", "charset")
+	title, _ := c.GetString("site", "title")
+	author, _ := c.GetString("site", "author")
+	icon, _ := c.GetString("site", "icon")
+	column, _ := c.GetString("site", "column")
+	root, _ := c.GetString("site", "root")
+	fmt.Println(charset, title, author, icon, column, root)
+}
+
+func main() {
+	fmt.Println("TaiChi Panda SiteGenerater")
+	readCfg("../../template/site.cfg")
+}
